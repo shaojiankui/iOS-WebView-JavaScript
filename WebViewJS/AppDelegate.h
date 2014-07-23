ee33bab004a9603eeb60237d343d5ef1bc406de3
Jakey <i@skyfox.org>
2014-07-23 14:43:42 +0800

    Initial Commit

---
+++ WebViewJS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebViewJS
+//
+//  Created by Jakey on 14-7-23.
+//  Copyright (c) 2014年 Jakey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
